8616829148c1222bc8bd2513736829595c4360f3
ArtunKARA <artunankara@gmail.com>
2024-03-12 15:02:56 +0300

    admin page update

--- ciapp/app/Controllers/Admin.php
+++ ciapp/app/Controllers/Admin.php
@@ -6,6 +6,8 @@ class Admin extends BaseController
 {
     public function index(): string
     {
-        return View('admin/adminHeader.php');
+        return View('admin/adminHeader.php')
+        .View('admin/adminHome.php')
+        .View('admin/adminFooter.php');
     }
 }
