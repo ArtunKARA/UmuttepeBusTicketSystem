3ee7c93fe6fdab0d701ee053f68054752ba8eebf
ArtunKARA <artunankara@gmail.com>
2024-03-14 13:25:40 +0300

    admin otobüs database

--- ciapp/app/Config/Routes.php
+++ ciapp/app/Config/Routes.php
@@ -5,5 +5,14 @@ use CodeIgniter\Router\RouteCollection;
 /**
  * @var RouteCollection $routes
  */
-$routes->get('/', 'Home::index');
+
+ //User Routes
+ $routes->get('/', 'Home::index');
+
+ //Admin Routes
 $routes->get('admin', 'Admin::index');
+
+$routes->get('admin/otobus', 'Admin::Otobus');
+$routes->get('admin/otobus/(:num)', 'AracTanimlari\Otobus::show/$1');
+$routes->post('admin/otobus', 'AracTanimlari\Otobus::create');
+$routes->delete('admin/otobus/(:num)', 'AracTanimlari\Otobus::delete/$1');
---
+++ ciapp/app/Views/admin/AracTanimlari/Otobus/index.php
@@ -0,0 +1,45 @@
+<!-- PAGE CONTENT -->
+<div class="content-wrapper">
+    <div class="content">
+
+
+        <div class="row">
+            <div class="col-lg-12">
+                <div class="card">
+                    <div class="card-block">
+                        <h6 class="card-title text-bold">Routes</h6>
+                        <p class="content-group">
+                            This is the most basic example of the datatables with zero configuration. Use the <code>.datatable</code> class to initialize datatables.
+                        </p>
+                        <table class="display datatable table table-stripped" cellspacing="0" width="100%">
+                            <thead>
+                                <tr>
+                                    <th>Kayıt No</th>
+                                    <th>Destinations</th>
+                                    <th>İsim</th>
+                                    <th>Plaka</th>
+                                    <th>Koltuk Sayisi</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                <?php foreach ($otobusler as $otobus) : ?>
+                                    <tr>
+                                        <td><?php echo $otobus['ID']; ?></td>
+                                        <td><?php echo $otobus['Aktif']; ?></td>
+                                        <td><?php echo $otobus['Isim']; ?></td>
+                                        <td><?php echo $otobus['Plaka']; ?></td>
+                                        <td><?php echo $otobus['KoltukSayisi']; ?></td>
+                                        <td>
+                                            
+                                        </td>
+                                    </tr>
+                                <?php endforeach; ?>
+                            </tbody>
+                        </table>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+<!-- /PAGE CONTENT -->
