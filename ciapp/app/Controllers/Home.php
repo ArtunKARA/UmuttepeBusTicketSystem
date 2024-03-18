ae0f42fa7dd6154787370465b2484aaa8ec32c10
ArtunKARA <artunankara@gmail.com>
2024-03-18 22:15:35 +0300

    login sayfalar entegrasyonu

--- ciapp/app/Controllers/Home.php
+++ ciapp/app/Controllers/Home.php
@@ -8,6 +8,7 @@ class Home extends BaseController
 {
     public function index(): string 
     {
+        $user = session();
         return View('kullanici/kullaniciHeader.php')
               .View('kullanici/KullaniciSeferSorgu.php')
               .View('kullanici/kullaniciGirisYap.php')
@@ -59,6 +60,7 @@ class Home extends BaseController
 
     public function admin(): string
     {
+        $user = session();
         return View('admin/adminHeader.php')
         .View('admin/adminHome.php')
         .View('admin/adminFooter.php');
